7d0ca3bc5e4ddf2c9f86b13f5109b08a36a2112b
Vestue <61783376+Vestue@users.noreply.github.com>
2023-06-04 21:51:16 +0200

    Shrinking rings

--- first_attempt.glsl
+++ first_attempt.glsl
@@ -3,6 +3,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
     
     float centerDistance = length(uv);
+    centerDistance = sin(centerDistance * 8. + iTime)/8.;
+    centerDistance = abs(centerDistance);
+    centerDistance = smoothstep(0.0, 0.1, centerDistance);
 
     fragColor = vec4(centerDistance, centerDistance, centerDistance, 1.0);
 }
