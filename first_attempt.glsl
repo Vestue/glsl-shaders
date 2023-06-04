d1e650e3061c12044965f19b0b90ac75ac6c4be9
Vestue <61783376+Vestue@users.noreply.github.com>
2023-06-04 22:02:20 +0200

    Add colored layer

--- first_attempt.glsl
+++ first_attempt.glsl
@@ -2,10 +2,14 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
     
-    float centerDistance = length(uv);
-    centerDistance = sin(centerDistance * 8. + iTime)/8.;
-    centerDistance = abs(centerDistance);
-    centerDistance = smoothstep(0.0, 0.1, centerDistance);
+    float d = length(uv);
+    d = sin(d * 8. + iTime)/8.;
+    d = abs(d);
+    
+    d = 0.02 / d;    
+    
+    vec3 colorLayer = vec3(0.0, 1.0, 1.0);
+    colorLayer *= d;
 
-    fragColor = vec4(centerDistance, centerDistance, centerDistance, 1.0);
+    fragColor = vec4(colorLayer, 1.0);
 }
