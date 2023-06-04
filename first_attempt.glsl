4724b75511c378312de11c08241749b47e3ec86b
Vestue <61783376+Vestue@users.noreply.github.com>
2023-06-04 22:23:51 +0200

    Fix colorshift

--- first_attempt.glsl
+++ first_attempt.glsl
@@ -1,9 +1,9 @@
 vec3 palette( float t ) 
 {
     vec3 a = vec3(0.5, 0.5, 0.5);
-    vec3 b = vec3(-0.562, -0.342, 0.158);
-    vec3 c = vec3(1.118, 1.0, 2.648);
-    vec3 d = vec3(-1.052, 0.333, 0.667);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(0.8, 0.8, 0.5);
+    vec3 d = vec3(0.0, 0.2, 0.5);
     
     return a + b * cos(6.28318 * (c * t + d));
 }
@@ -13,12 +13,13 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
     
     float d = length(uv);
+    vec3 colors = palette(d + iTime);
     d = sin(d * 8. + iTime)/8.;
     d = abs(d);
     
     d = 0.02 / d;    
     
-    vec3 colors = palette(d + iTime);
+    
     colors *= d;
 
     fragColor = vec4(colors, 1.0);
