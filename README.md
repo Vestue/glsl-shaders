ee0042108452cb22cc94cc4fa9d51a6806fcf174
Vestue <61783376+Vestue@users.noreply.github.com>
2023-06-04 22:17:55 +0200

    Make colors use trigonometry based palette

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # glsl-shaders
+
 Experimental GLSL shaders created using https://www.shadertoy.com
+
+The color palettes are created using [dev.thi.ng gradients](https://github.com/thi-ng/cgg)
--- first_attempt.glsl
+++ first_attempt.glsl
@@ -1,3 +1,13 @@
+vec3 palette( float t ) 
+{
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(-0.562, -0.342, 0.158);
+    vec3 c = vec3(1.118, 1.0, 2.648);
+    vec3 d = vec3(-1.052, 0.333, 0.667);
+    
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
@@ -8,8 +18,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     
     d = 0.02 / d;    
     
-    vec3 colorLayer = vec3(0.0, 1.0, 1.0);
-    colorLayer *= d;
+    vec3 colors = palette(d + iTime);
+    colors *= d;
 
-    fragColor = vec4(colorLayer, 1.0);
+    fragColor = vec4(colors, 1.0);
 }
