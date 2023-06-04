2c0fe896c6acd0b10bfd492445ce77252d39709e
Vestue <61783376+Vestue@users.noreply.github.com>
2023-06-04 21:40:52 +0200

    Centered and weighed

--- first_attempt.glsl
+++ first_attempt.glsl
@@ -1,11 +1,6 @@
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
+    vec2 uv = fragCoord / iResolution.xy * 2.0 - 1.0;
 
-    // Time varying pixel color
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
-
-    // Output to screen
-    fragColor = vec4(col,1.0);
+    fragColor = vec4(uv, 0.0, 1.0);
 }
