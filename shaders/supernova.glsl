a85ecb50e77a8312288ab74fa42b930e64a69671
Vestue <61783376+Vestue@users.noreply.github.com>
2023-06-05 11:48:35 +0200

    Move shaders into directory

---
+++ shaders/supernova.glsl
@@ -0,0 +1,34 @@
+vec3 palette( float t ) 
+{
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(-0.562, -0.342, 0.158);
+    vec3 c = vec3(1.118, 1.0, 2.648);
+    vec3 d = vec3(-1.052, 0.333, 0.667);
+    
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+    
+    for (int i = 0; i < 4; i++)
+    {
+        uv = fract(uv * 0.3) - 0.5;
+
+        float d = length(uv) * exp(-length(uv0));
+        vec3 colors = palette(length(uv0) + iTime*.8);
+
+        d = sin(d * 2. + iTime)/10.;
+        d = abs(d);
+        d = pow(0.03 / d, 5.0);    
+
+
+        finalColor += colors * d;
+    }
+    
+
+    fragColor = vec4(finalColor, 1.0);
+}
